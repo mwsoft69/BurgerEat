cc5cffbb05e6522490cd7cb4824b6e0c75d17a10
mitch <mitch@boxey>
2020-07-14 16:51:32 -0500

    Rework main.c

--- main.c
+++ main.c
@@ -6,30 +6,20 @@
 
 #include <SDL.h>
 #include <SDL_image.h>
-#include <stdio.h>
+
 #include <stdbool.h>
 #include <stdlib.h>
 #include <time.h>
 
 #include "image.h"
+#include "npc.h"
+#include "player.h"
+#include "init.h"
 
-/*Prob not the best way but its public domain. let me know a better way !! :)*/
-int initWindow(SDL_Window **s,const char *title, int xpos, int ypos, int x, int y)
+typedef enum
 {
-	*s = SDL_CreateWindow(title,xpos,ypos,x,y,0);
-	
-	if(!*s)
-	{
-		SDL_ShowSimpleMessageBox(0,"ERROR!!","Failed to init window",0);
-		SDL_DestroyWindow(s);
-		s= NULL;
-		SDL_Quit();
-		exit(EXIT_FAILURE);
-	}
-
-	
-}
-
+	UP,DOWN,LEFT,RIGHT
+}keyPress;
 
 void endCleanup(SDL_Window *w,SDL_Renderer *r)
 {
@@ -38,45 +28,29 @@ void endCleanup(SDL_Window *w,SDL_Renderer *r)
 	SDL_Quit();
 }
 
-
-
-void initPlayerPos(SDL_Rect *pos,int x,int y,int w,int h)
+void movePlayer(keyPress k,SDL_Rect *r,int vol)
 {
-		pos->x=x;
-		pos->y=y;
-		pos->w=w;
-		pos->h=h;
+	if(k == UP)
+	{
+		r->y -= vol;
+	}
+
+	if(k == DOWN)
+	{
+	r->y += vol;
+	}
+
+	if(k==LEFT)
+	{
+		r->x -= vol;
+	}
+
+	if(k==RIGHT)
+	{
+		r->x += vol;
+	}
 }
 
-void initBurgerPos(SDL_Rect *bPos,int screenH,int screenW)
-{
-	/*Create timer.*/
-	//SDL_TimerID waitTime;
-
-	//Uint32 delay = 
-
-	/*"Randomly" place the burger sprite. rand() and srand are fine for this*/
-	
-	/*Set height and width of sprite.*/
-	bPos->h=64;
-	bPos->w=64;
-
-	/*seed random numbers*/
-	srand(time(NULL));
-	
-	int wRand,hRand,dRand = 0;
-
-	hRand = (rand()%bPos->h/screenH);
-	wRand = (rand()%bPos->w/screenW);
-	dRand = (rand()%5000)+1000;
-
-	/*stuff idk if it works*/
-	//SDL_Delay(dRand);
-	bPos->x = wRand;
-	bPos->y = hRand;
-
-
-}
 
 int main(int argc, char ** argv)
 {
@@ -84,50 +58,62 @@ int main(int argc, char ** argv)
 	/*Define event "boolean" and SDL_Event */
 	bool quit = false;
 	SDL_Event  e;
-	int screenY = 480;
-	int screenX = 640;
+
+	/*Players score.*/
+
+	int *playerScore = 0;
+
+	/**/
+	int screenW = 480;
+	int screenH = 640;
 	char *title ="My Awesome Game!";
 	SDL_Renderer *ren = NULL;
 	SDL_Window *screen = NULL;
 	SDL_Surface *npc = NULL;
 	SDL_Surface *player = NULL;
-	
+	SDL_Surface *bg = NULL;
+
 	/*NPC texture*/
-	SDL_Texture *t1;
-	
+	SDL_Texture *t1 = NULL;
+
 	/*Player texture*/
-	SDL_Texture *t2;
-	
+	SDL_Texture *t2 = NULL;
+
+	/*Background texture.*/
+	SDL_Texture *bgTexture = NULL;
+
 	SDL_Rect cbNpcRect;
 	SDL_Rect cbPlayerRect;
-	
-	initPlayerPos(&cbPlayerRect,50,80,64,64);
+
+	initPlayerPos(&cbPlayerRect,50,80,83,59);
 
 	SDL_Init(SDL_INIT_EVERYTHING);
 
-	initWindow(&screen,title,SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,screenX,screenY);
+	initWindow(&screen,title,SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,screenW,screenH);
 
 	initIMG();
-	
+
 	/*Create our renderer*/
-	
+
 	ren = SDL_CreateRenderer(screen,-1,SDL_RENDERER_ACCELERATED);
-	
+
 	/*load bmp*/
 
 	loadPNG(&npc,"art/CheeseburgerAlpha.png");
-	
-	loadPNG(&player,"art/CheeseburgerAlpha.png");
+
+	loadPNG(&player,"art/CheeseburgerAlphaCrop.png");
+
+	loadPNG(&bg,"art/Space003.png");
 
 	convertToTexture(&t1,ren,player);
 	convertToTexture(&t2,ren,npc);
-	
-	
+	convertToTexture(&bgTexture,ren,bg);
+
 	/*Main game loop.*/
-	
+
 	while(!quit)
-	{	
-		/*Event loop.*/	
+	{
+		/*Event loop.*/
 		while(SDL_PollEvent(&e) !=0 )
 		{
 
@@ -142,48 +128,66 @@ int main(int argc, char ** argv)
 				switch (e.key.keysym.sym)
 				{
 					case SDLK_UP:
-					
-					cbPlayerRect.y -= 10;
+
+					movePlayer(UP,&cbPlayerRect,10);
+					printf("Score is %d\n",playerScore);
+					//cbPlayerRect.y -= 10;
 					break;
-					
+
 					case SDLK_DOWN:
-					
-					cbPlayerRect.y += 10;
+
+					movePlayer(DOWN,&cbPlayerRect,10);
 					break;
-					
+
 					case SDLK_LEFT:
-					
-					cbPlayerRect.x -= 10;
+
+					movePlayer(LEFT,&cbPlayerRect,10);
 					break;
-					
+
 					case SDLK_RIGHT:
-					
-					cbPlayerRect.x += 10;
+
+					movePlayer(RIGHT,&cbPlayerRect,10);
 					break;
 				}
-				 
+
 			}
+			playerEdgeColl(&cbPlayerRect,screenH,screenW,&playerScore);
+
 		}
 
 		/*Do all drawing and logic here*/
-		initBurgerPos(&cbNpcRect,screenX,screenY);
+		initBurgerPos(&cbNpcRect,screenW,screenH);
 		SDL_RenderClear(ren);
+
+		/*Add delays to slow down the game and reduce CPU usage.*/
+
+		/*Render our background.*/
+		SDL_RenderCopy(ren,bgTexture,NULL,NULL);
+
+
 		SDL_RenderCopy(ren,t2,NULL,&cbNpcRect);
-		SDL_Delay(33);
+
 		SDL_RenderCopy(ren,t1,NULL,&cbPlayerRect);
-		SDL_Delay(33);
+
+
 		SDL_RenderPresent(ren);
 		SDL_Delay(33);
+
+
 	}
-	
-	
+
+
 	/*Free memory and quit.*/
 	SDL_FreeSurface(player);
 	SDL_FreeSurface(npc);
 	SDL_DestroyTexture(t1);
 	SDL_DestroyTexture(t2);
+	SDL_DestroyTexture(bgTexture);
+	SDL_FreeSurface(bg);
+	IMG_Quit();
 	endCleanup(screen,ren);
-	
+
+
 	quit = true;
 	return 0;
-}	
+}
